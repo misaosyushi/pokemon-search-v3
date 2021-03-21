e8690243d918b500f93551e20800fdeded7b48a8
misaosyushi <misaki.tsujita@rakus-partners.co.jp>
2021-03-21 11:21:48 +0900

    wip

--- src/DotnetTestFunction/Function.cs
+++ src/DotnetTestFunction/Function.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
+using Amazon.DynamoDBv2.DocumentModel;
 using Amazon.Lambda.Core;
+using Amazon.Runtime;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -12,6 +16,9 @@ namespace DotnetTestFunction
 {
     public class Function
     {
+        
+        private static readonly AmazonDynamoDBClient client = new AmazonDynamoDBClient();
+        private static readonly string tableName = "PokemonTable";
         
         /// <summary>
         /// A simple function that takes a string and does a ToUpper
